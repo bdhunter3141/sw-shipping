8003c69e9f31c45c165b60585f82aed2a4fe7bf2
Bryan Hunter <bdhunter3141@gmail.com>
2019-07-17 20:03:01 -0400

    Now fetching data from SWAPI and mapping list of ten starships to page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from "react";
+import uuid from "uuid/v1";
 
 function App() {
-  const [starships, setStarships] = useState([{name: "Millenium Falcon", id: 1}, {name: "Executor", id: 2}, {name: "Sentinel-class landing craft", id: 3},]);
+  const [starships, setStarships] = useState([]);
+  const addStarships = starshipJson => {
+    const starshipList = [];
+    starshipJson.results.forEach(starship => {
+      starshipList.push({ name: starship.name, id: uuid() });
+    });
+    setStarships([...starships, ...starshipList]);
+  };
+
+  useEffect(() => {
+    if (!starships.length) {
+      fetch("https://swapi.co/api/starships")
+        .then(response => response.json())
+        .then(starshipJson => {
+          addStarships(starshipJson);
+        });
+    }
+  });
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>SW Shipping</h1>
       <h2>Our Fleet</h2>
       <ul>
-      {starships.map(starship => (<li key={starship.id}>{starship.name}</li>))}
+        {starships.length ? (
+          starships.map(starship => <li key={starship.id}>{starship.name}</li>)
+        ) : (
+          <p>Loading...</p>
+        )}
       </ul>
     </div>
   );
